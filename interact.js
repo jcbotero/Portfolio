5c748f9c68128cec48c23d34d9342d8107c93d91
Juan Botero <jucaob10@hotmail.com>
2022-12-16 17:09:05 -0500

    me

---
+++ interact.js
@@ -0,0 +1,14 @@
+
+
+setTimeout(function(){
+    document.getElementById('imp').style.display = "block";
+   }, 2000); /* asi hago aparecer un div despues de 2 seconds*/
+
+setTimeout(function(){
+    document.getElementById('kep').style.display = "block";
+   }, 4000);
+
+ setTimeout(function(){
+    document.getElementById('ach').style.display = "block";
+   }, 6000);
+   
