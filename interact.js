7de9501dae5a6013b5f12c055d13cfe70059e8c6
Juan Camilo Botero <97107156+jcbotero@users.noreply.github.com>
2022-12-20 17:14:13 -0500

    Add files via upload

--- interact.js
+++ interact.js
@@ -13,5 +13,19 @@ setTimeout(function(){
    }, 6000);
 
 
+   function siteOne() {
+    location.href = "https://jcbotero.github.io/Advice-generator/";
+  }
    
+  function siteTwo() {
+    location.href = "https://jcbotero.github.io/Tip-Calculator/";
+  }
    
+  
+  function siteThree() {
+    location.href = "https://jcbotero.github.io/E-commerce-product-page/";
+  }
+  
+  function siteFour() {
+    location.href = "https://jcbotero.github.io/c-card-form/";
+  }
